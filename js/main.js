043d596d4d3c6ab15815104cc7bc23b046f2c9ed
JohnMark Fisher <fisherjohnmark@gmail.com>
2019-06-03 11:01:11 -0400

    Updates

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 function initializeMap() {
     
-    var map = L.map('map');
-    
     var barHeight = $('#bar').height();
     var windowHeight = $(window).innerHeight();
     
     var mapHeight = (windowHeight-barHeight)-16;
     
     $('#map').height(mapHeight);
+    
+    var map = L.map('map', {
+        center: [40.638908, -73.968429],
+        zoom: 13
+    });
     
     var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',{
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> and <a href="https://carto.com/attributions">CARTO</a>',
@@ -17,47 +20,74 @@ function initializeMap() {
     
     var getURL = 'https://fisherjohnmark.carto.com/api/v2/sql?format=GeoJSON&q=';
     var sql = 'SELECT the_geom, city, location, time FROM fisherjohnmark.uniwatch&api_key=default_public';
-    
-    var partyMarker = {
-        radius: 7,
-        color: "#156736",
-        fillOpacity: 0.75
-    };
 
     $.getJSON(getURL+sql, function(data){
         
         var features = data.features;
-        console.log(features)
         
         var allParties = [];
         
         var parties = L.geoJSON(features, {
             pointToLayer: function (feature, latlng) {
                 allParties.push(latlng);
-                return L.circleMarker(latlng,partyMarker);
+                return customPoints(feature,latlng);
             },
-            onEachFeature: playerData
+            onEachFeature: partyData
         }).addTo(map);
         
         var bounds = L.latLngBounds(allParties);
         
-        map.fitBounds(bounds);
+        function flyBounds(){
+            map.flyToBounds(bounds);
+        };
+        
+        setTimeout(flyBounds, 2000)
 
     });
 
 }; // end initializeMap
 
-function playerData(feature,layer){
+function customPoints(feature,latlng) {
     
-    var popupContent = "CITY: "+feature.properties.city+"<br>LOCATION: "+feature.properties.location+"<br>TIME: "+feature.properties.time;
+    if (feature.properties.city == "Brooklyn, NY") {
+        
+        var greenIcon = L.icon({
+            iconUrl: 'img/confetti.svg',
+            shadowUrl: 'img/confettishadow.svg',
+            iconSize: [38, 95],
+            shadowSize: [40, 97]
+        });
+        
+        return L.marker(latlng, {icon: greenIcon});
+        
+    } else {
+        
+        var partyMarker = {
+            radius: 7,
+            color: "#156736",
+            fillOpacity: 0.75
+        };
+
+        return L.circleMarker(latlng,partyMarker);
+    };
+
+}; // end customPoints
+
+function partyData(feature,layer){
+    
+    if (feature.properties.time !== ''){
+        
+        var popupContent = "CITY: "+feature.properties.city+"<br>LOCATION: "+feature.properties.location+"<br>TIME: "+feature.properties.time;
+        
+    } else {
+        
+        var popupContent = "CITY: "+feature.properties.city+"<br>LOCATION: "+feature.properties.location;
+        
+    };
     
     layer.bindTooltip(popupContent, {
-        offset: [0,-7],
-        direction: 'top',
         className: 'popupPlayer'});
     
-    $('#zoom-select').append($('<option></option>').attr('value', feature.properties.city).text(feature.properties.city));
-    
-}; // end of playerData
+}; // end of partyData
 
 $(document).ready(initializeMap);
